b1dcce6dd7ea01cdd7b47ac4baf831ee118b1910
Ralu <ralufajobi@outlook.com>
2022-11-01 12:28:55 +0000

    Third commit - updated directory

--- localState.js
+++ localState.js
@@ -1,4 +1,4 @@
 // needed to store users data via device rather than account
 
-// need to add functionalitu that allows user to pick whichever lesson they want to do, 
+// need to add functionality that allows user to pick whichever lesson they want to do, 
 // could forward the user to the quiz navigaiton 
